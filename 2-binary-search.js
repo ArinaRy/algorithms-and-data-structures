e575985e6f997e2e06438f8a60fdca86534a2f06
Arina Ryazanova <arina-ryazanovaa@mail.ru>
2023-10-04 19:55:27 +0300

    Завершена работа над реализацией бинарного поиска

--- 2-binary-search.js
+++ 2-binary-search.js
@@ -8,12 +8,22 @@ function binarySearch(array, item) {
     let found = false;
     let position = -1;
     while (found === false && start <= end) {
+        count += 1;
         middle = Math.floor((start + end)/2);
         if (array[middle] == item) {
             found = true;
-            position 
+            position = middle;
+            return position;
+        }
+        if (item < array[middle]) {
+            end = middle - 1;
+
+        } else {
+            start = middle + 1;
         }
     }
+    return position;
 }
 
+console.log(binarySearch(array, 3))
 console.log(count)
